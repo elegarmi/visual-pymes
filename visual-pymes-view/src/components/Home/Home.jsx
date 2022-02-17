e6e0f467907ebbecf459f9470ca33afd32ba903d
elegarmi <elegarmi@gmail.com>
2022-02-17 18:23:24 +0100

    copied files from old repository

--- visual-pymes-view/src/components/Home/Home.jsx
+++ visual-pymes-view/src/components/Home/Home.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import { products } from "./data.js";
 import Product from "./Product/Product.jsx";
-/* import Searchbar from "./Searchbar/Searchbar"; */
 import "./Home.css";
-import Create from "./Product/Create.jsx";
 
 const Home = () => {
   return (
     <div className="home">
-      {/*     <Searchbar  onChange={filterName}/> */}
       <article className="home__cards">
         {products.map((product) => (
           <Product
             id={product.id}
+            key={product.id}
             name={product.name}
             image={product.image}
             price={product.price}
           />
         ))}
       </article>
-          <Create />
     </div>
   );
 };
