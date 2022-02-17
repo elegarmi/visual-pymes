f2fb82b94f2ccbe1e9cc3479a02310fd99da0297
elegarmi <elegarmi@gmail.com>
2022-02-17 22:55:58 +0100

    makeorder issue solved

--- visual-pymes-view/src/components/Home/MakeOrder.jsx
+++ visual-pymes-view/src/components/Home/MakeOrder.jsx
@@ -23,20 +23,3 @@ const MakeOrder = () => {
   };
   
 export default MakeOrder;
-
-
-const CreateView = () => {
-  return (
-    <div className="create container">
-      <div className="row">
-        <div className="col col-lg-6 col-xs-12">
-          <h2>Gracias por confiar en <span className="text-yellow">Visual</span><span className="text-green">Pymes</span></h2>
-          <p>Rellena el formulario y disfruta de los beneficios</p>        
-        </div>
-        <Create  title="Crear nuevo anuncio" input1="Nombre del producto" 
-          input2="Precio (0,00 €)" input3="Descripción"
-          nameButton= "Crear"/>
-      </div>
-    </div>
-  );
-};
